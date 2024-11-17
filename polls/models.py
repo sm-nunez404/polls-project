603a65496be346b99329c033b7be729f8e965264
smnunez404 <smnunez404@gmail.com>
2024-11-17 13:48:28 -0400

    Header Polls administration agregado en el header

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,5 @@
+from django.contrib import admin
+
 import datetime
 
 from django.db import models
@@ -8,6 +10,13 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     
+    
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+        
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
